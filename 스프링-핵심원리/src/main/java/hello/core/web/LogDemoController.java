012d415e86763debe2543e992f2f0adca29cafa2
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-01-24 17:53:14 +0900

    no message

--- 스프링-핵심원리/src/main/java/hello/core/web/LogDemoController.java
+++ 스프링-핵심원리/src/main/java/hello/core/web/LogDemoController.java
@@ -1,2 +1,37 @@
-package hello.core.web;public class LogDemoController {
+package hello.core.web;
+
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    //HttpServletRequest 를받으면 고객 요청 정보를 받을수있음
+    public String logDemo(HttpServletRequest request){
+
+        String requestURL = request
+                .getRequestURL()
+                .toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.losic("testId");
+        return "OK";
+    }
+
+
+
 }
