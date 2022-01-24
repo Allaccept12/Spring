b16afda781d84a986be460ff661666dbaa3cc26f
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-01-24 16:34:47 +0900

    [PUT] Prototype Bean 사용시 문제점과 해결

--- 스프링-핵심원리/src/main/java/hello/core/AppConfig.java
+++ 스프링-핵심원리/src/main/java/hello/core/AppConfig.java
@@ -15,29 +15,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-
-    @Bean
-    //멤버서비스
-    public MemberService memberService(){
-        System.out.println("call AppConfig.memberService");
-        return new MemberServiceImple(getMemberRepository());
-    }
-    @Bean
-    //오더서비스
-    public OrderService orderService(){
-        System.out.println("call AppConfig.orderService");
-        return new OrderServiceImpl(getMemberRepository(), discountPolycy());
-        //return null
-    }
-    @Bean
-    //레파지토리
-    public MemberRepository getMemberRepository() {
-        System.out.println("call AppConfig.getMemberRepository");
-        return new MemoryMemberRepository();
-    }
-    @Bean
-    //할인정책
-    public DiscountPolycy discountPolycy(){
-        return new RateDiscountPolycy();
-    }
+//    @Bean
+//    //멤버서비스
+//    public MemberService memberService(){
+//        System.out.println("call AppConfig.memberService");
+//        return new MemberServiceImple(getMemberRepository());
+//    }
+//    @Bean
+//    //오더서비스
+//    public OrderService orderService(){
+//        System.out.println("call AppConfig.orderService");
+//        return new OrderServiceImpl(getMemberRepository(), discountPolycy());
+//        //return null
+//    }
+//    @Bean
+//    //레파지토리
+//    public MemberRepository getMemberRepository() {
+//        System.out.println("call AppConfig.getMemberRepository");
+//        return new MemoryMemberRepository();
+//    }
+//    @Bean
+//    //할인정책
+//    public DiscountPolycy discountPolycy(){
+//        return new RateDiscountPolycy();
+//    }
 }
--- 스프링-핵심원리/src/test/java/hello/core/scope/SingletonPrototypeTest1.java
+++ 스프링-핵심원리/src/test/java/hello/core/scope/SingletonPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonPrototypeTest1 {
 
@@ -38,23 +40,22 @@ public class SingletonPrototypeTest1 {
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
         //새로운 프로토타입이어야하는데 이미 싱글톤 빈에 있는 값을 그대로 가져와 카운트를 올린다. 
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
 
     }
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; //생성시점에 주입
-
+//        private final PrototypeBean prototypeBean; //생성시점에 주입
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
-
         }
     }
 
