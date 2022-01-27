509b708cad4188bf53c6125d0c3cfe7e7a8485cc
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-01-27 16:50:16 +0900

    [ADD] Spring MVC 로 웹앱 구현

---
+++ 스프링-MVC/sringmvc/src/main/java/hello/sringmvc/SringmvcApplication.java
@@ -0,0 +1,13 @@
+package hello.sringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringmvcApplication.class, args);
+	}
+
+}
---
+++ 스프링-MVC/sringmvc/src/test/java/hello/sringmvc/SringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.sringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
