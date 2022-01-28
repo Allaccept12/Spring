837498f0eadea469ff56866a804f67be817f8e39
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-01-28 21:12:18 +0900

    [ADD] HTTP 응답 정리

--- 스프링-MVC/sringmvc/src/main/java/hello/sringmvc/basic/requestmapping/RequestBodyJsonController.java
+++ 스프링-MVC/sringmvc/src/main/java/hello/sringmvc/basic/requestmapping/RequestBodyJsonController.java
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.sringmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -61,6 +59,7 @@ public class RequestBodyJsonController {
         log.info("username = {} , age = {}", helloData.getUsername(),helloData.getAge());
         return "ok";
     }
+
     @ResponseBody
     @PostMapping("/request-body-json-v5")
     public HelloData requestBodyJsonV5(@RequestBody HelloData data)  {
