ef613acdb809224845abaeb6b9ab9d6c45ca1743
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-01-30 17:39:35 +0900

    [PATCH] 상품 정보 수정

--- 스프링-MVC/item-service/src/main/java/hello/itemservice/web/basic/BasicItemContoller.java
+++ 스프링-MVC/item-service/src/main/java/hello/itemservice/web/basic/BasicItemContoller.java
@@ -42,19 +42,21 @@ public class BasicItemContoller {
     public String addForm() {
         return "/basic/addForm";
     }
+
     //@PostMapping("/add")
     public String addItemV1(@RequestParam String itemName,
-                       @RequestParam int price,
-                       @RequestParam Integer quantity,
-                       Model model) {
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
         Item item = new Item();
         item.setItemName(itemName);
         item.setPrice(price);
         item.setQuantity(quantity);
         itemRepository.save(item);
-        model.addAttribute("item",item);
+        model.addAttribute("item", item);
         return "/basic/item";
     }
+
     @PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item, Model model) {
         itemRepository.save(item);
@@ -62,5 +64,16 @@ public class BasicItemContoller {
         return "/basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId,item);
 
+        return "redirect:/basic/items/{itemId}";
+    }
 }
